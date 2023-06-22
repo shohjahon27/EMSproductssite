a8dad83fc6a7aeffa60acb6eb98d18b71a890702
shohjahon27 <xolpolatovshohjahon5@gmail.com>
2023-06-22 09:35:20 +0500

    aksiya added

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,71 +103,7 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
 
-// TIMER PROMOTION
-const deadline = "2023-5-30";
 
-function getTimeRemaining(endtime) {
-  let days, hours, minutes, seconds;
-  const timer = Date.parse(endtime) - Date.parse(new Date());
-
-  if (timer <= 0) {
-    days = "0";
-    hours = "0";
-    minutes = "0";
-    seconds = "0";
-  } else {
-    (days = Math.floor(timer / (1000 * 60 * 60 * 24))),
-      (hours = Math.floor((timer / (1000 * 60 * 60)) % 24)),
-      (minutes = Math.floor((timer / 1000 / 60) % 60)),
-      (seconds = Math.floor((timer / 1000) % 60));
-  }
-
-  return { timer, days, hours, minutes, seconds };
-}
-
-function getZero(num) {
-  if (num >= 0 && num < 10) {
-    return `0${num}`;
-  } else {
-    return num;
-  }
-}
-
-function setClock(selector, endtime) {
-  const timer = document.querySelector(selector),
-    days = timer.querySelector("#days"),
-    hours = timer.querySelector("#hours"),
-    minutes = timer.querySelector("#minutes"),
-    seconds = timer.querySelector("#seconds"),
-    timeInterval = setInterval(updateClock, 1000);
-
-  updateClock();
-
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
-
-    (days.innerHTML = getZero(t.days)),
-      (hours.innerHTML = getZero(t.hours)),
-      (minutes.innerHTML = getZero(t.minutes)),
-      (seconds.innerHTML = getZero(t.seconds));
-
-    if (t.timer <= 0) {
-      clearInterval(timeInterval);
-    }
-  }
-}
-
-setClock(".timer", deadline);
-
-// close
-const x = document.querySelector(".close"),
-  pro = document.querySelector(".promotion");
-
-x.addEventListener("click", (e) => {
-  if (e.target == x) {
-    pro.style.display = "none";
-  }
-});
 
 // // MODAL
 // const modaltrigger = document.querySelector("[data-modal]"),
